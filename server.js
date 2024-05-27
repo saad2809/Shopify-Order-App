f52b141f45a072c0f2d9a510d241c954e28cecbc
Saad Ullah Sajid <saad.sajid@c2-digital.com>
2024-05-28 01:01:41 +0500

    serverless added

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// api/server.js
 const express = require('express');
-const cors = require('cors');
-const ordersRouter = require('./routes/orders');
-const customerRouter = require('./routes/customers');
-
+const serverless = require('serverless-http');
 const app = express();
+const ordersRouter = require('../src/routes/orders');
+const customerRouter = require('../src/routes/customers');
+const cors = require('cors');
+
 
 const allowlist = ['https://heartycajun.com', 'https://6a5476-3.myshopify.com', 'http://127.0.0.1:9292'];
 
@@ -19,11 +21,15 @@ var corsOptionsDelegate = function (req, callback) {
     callback(null, corsOptions); // callback expects two parameters: error and options
 };
 
-// Use cors middleware with options
-app.use(cors(corsOptionsDelegate)); 
+app.use(cors(corsOptionsDelegate));
 
-// Use routes
+app.use(express.json());
 app.use('/api/orders', ordersRouter);
 app.use('/api/customers', customerRouter);
 
+app.get('/', (req, res) => {
+  res.send('App is running..');
+});
+
 module.exports = app;
+module.exports.handler = serverless(app);
